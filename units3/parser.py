0ff3a55b602d70bf0273c7586dbb6225cf95d91b
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2014-07-12 00:01:42 +0200

    Implemented 'prenotazione_appelli' parser

--- units3/parser.py
+++ units3/parser.py
@@ -14,19 +14,24 @@ class Parser:
 
         # Skip the first line, garbage
         for row in rows_list[1:]:
-            esame = {'anno_di_corso': int(row.contents[1].string),
-                     'nome': row.contents[2].string.split(' - ')[1],
-                     'codice': row.contents[2].string.split(' - ')[0],
-                     'crediti': int(row.contents[7].string),
-                     'anno_frequenza': row.contents[9].string,
+            # Row in a nice list format
+            f_row = row.get_text('|', strip=True).split('|')
+
+            codice, nome = f_row[1].split(' - ')
+
+            esame = {'anno_di_corso': int(f_row[0]),
+                     'nome': nome,
+                     'codice': codice,
+                     'crediti': int(f_row[2]),
+                     'anno_frequenza': f_row[3],
                      }
 
             # Verify if "voto - data" is present, they could be not
-            if row.contents[10].string is None:
+            if len(f_row) < 5:
                 esame['superato'] = False
             else:
                 esame['superato'] = True
-                esame['voto'], esame['data'] = row.contents[10].string.split(u'\u00a0-\u00a0')
+                esame['voto'], esame['data'] = f_row[4].split(u'\u00a0-\u00a0')
 
             result.append(esame)
 
@@ -40,25 +45,49 @@ class Parser:
         # Skip the first line, garbage
         for row in rows_list[1:]:
             if len(row.contents) > 3:
-                # Convert to float the value
-                importo = float(row.contents[6].string[2:].replace(',', '.'))
-
                 # If there's the green semaphore the fee is payed
-                if ('semaf_v' in str(row.contents[7])):
-                    stato = 'pagato'
-                # Otherwise not
-                else:
-                    stato = 'da_pagare'
+                stato = 'pagato' if 'semaf_v' in str(row.contents[7]) else 'da_pagare'
 
-                tassa = {'codice_fattura': int(row.contents[1].string),
-                         'codice_bollettino': int(row.contents[2].string),
-                         'anno': row.contents[3].string,
-                         'descrizione': row.contents[4].string,
-                         'data_scadenza': row.contents[5].string,
+                # Row in a nice list format
+                f_row = row.get_text('|', strip=True).split('|')
+
+                # Convert to float the value
+                importo = float(f_row[5][2:].replace(',', '.'))
+
+                tassa = {'codice_fattura': int(f_row[0]),
+                         'codice_bollettino': f_row[1],
+                         'anno': f_row[2],
+                         'descrizione': f_row[3],
+                         'data_scadenza': f_row[4],
                          'importo': importo,
                          'stato': stato
                          }
 
                 result.append(tassa)
+
+        return result
+
+    def prenotazione_appelli(self):
+        rows_list = self.soup.find('table', {'class': 'detail_table'}).find_all('tr')
+
+        result = []
+
+        # Skip the first line, garbage
+        for row in rows_list[1:]:
+            stato = 'chiuso' if 'chiuse' in str(row.contents[1]) else 'aperto'
+
+            # Row in a nice list format
+            f_row = row.get_text('|', strip=True).split('|')
+
+            appello = {'nome_corso': f_row[0],
+                       'data': f_row[1],
+                       'inizio_periodo_iscrizione': f_row[2],
+                       'termine_periodo_iscrizione': f_row[3],
+                       'descrizione': f_row[4],
+                       'sessioni': f_row[5],
+                       'stato': stato
+                       }
+
+            result.append(appello)
 
         return result
