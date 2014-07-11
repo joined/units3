dd11748b59bab041f1f253b5bfb4e542c32214a8
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2014-07-11 14:21:32 +0200

    Incorporated debug option in run.py and added virtualenv check

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 #!.env/bin/python
 # -*- coding: UTF-8 -*- #
 from units3.main import app
+import sys
+
+if not hasattr(sys, 'real_prefix'):
+    print("Must run inside virtualenv.")
+    sys.exit(1)
 
 if __name__ == "__main__":
-    app.run()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="start in debug mode",
+                        action="store_true")
+    args = parser.parse_args()
+
+    app.run(debug=args.debug)
