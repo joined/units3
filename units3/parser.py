4ffe5017e6196f6f6d97efed15f427ae238db28a
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2014-07-11 18:11:29 +0200

    Small changes in JSON response format.

--- units3/parser.py
+++ units3/parser.py
@@ -14,19 +14,19 @@ class Parser:
 
         # Skip the first line, garbage
         for row in rows_list[1:]:
-            # Verify if "voto - data" is present, they could be not
-            if row.contents[10].string is None:
-                voto, data = None, None
-            else:
-                voto, data = row.contents[10].string.split(u'\u00a0-\u00a0')
-
-            esame = {'anno_di_corso': row.contents[1].string,
+            esame = {'anno_di_corso': int(row.contents[1].string),
                      'nome': row.contents[2].string.split(' - ')[1],
                      'codice': row.contents[2].string.split(' - ')[0],
-                     'crediti': row.contents[7].string,
+                     'crediti': int(row.contents[7].string),
                      'anno_frequenza': row.contents[9].string,
-                     'voto': voto,
-                     'data': data}
+                     }
+
+            # Verify if "voto - data" is present, they could be not
+            if row.contents[10].string is None:
+                esame['superato'] = False
+            else:
+                esame['superato'] = True
+                esame['voto'], esame['data'] = row.contents[10].string.split(u'\u00a0-\u00a0')
 
             result.append(esame)
 
@@ -50,8 +50,8 @@ class Parser:
                 else:
                     stato = 'da_pagare'
 
-                tassa = {'codice_fattura': row.contents[1].string,
-                         'codice_bollettino': row.contents[2].string,
+                tassa = {'codice_fattura': int(row.contents[1].string),
+                         'codice_bollettino': int(row.contents[2].string),
                          'anno': row.contents[3].string,
                          'descrizione': row.contents[4].string,
                          'data_scadenza': row.contents[5].string,
