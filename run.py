86533bbec5b20370b249a0b291f98747102e20a3
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2014-07-14 23:09:18 +0200

    Add option to make server externally visible in run.py

--- run.py
+++ run.py
@@ -11,8 +11,16 @@ from units3.main import app
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug", help="start in debug mode",
+    parser.add_argument("-d", "--debug",
+                        help="start in debug mode",
                         action="store_true")
+
+    parser.add_argument("-v", "--visible",
+                        help="make server externally visible",
+                        action="store_true")
+
     args = parser.parse_args()
+
+    host = '0.0.0.0' if args.visible else '127.0.0.1'
 
     app.run(debug=args.debug)
