cd6a6e10cfa15ce41adf1537347ec5bc7e6cef03
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2014-07-20 21:02:49 +0200

    Cleaned parsers.

--- units3/parser.py
+++ units3/parser.py
@@ -13,18 +13,21 @@ class Parser:
         result = []
 
         for table in tables:
-            nome_corso, codice, descrizione = table.xpath('tr/th/text()')[0] \
-                .split(' - ')
+            nome_corso, codice, descrizione = \
+                table.xpath('tr/th/text()')[0].split(' - ')
+
             numero_iscrizione = table.xpath('tr/th/a/text()')[0].split(': ')[1]
 
-            date = table.xpath('tr[6]/td[1]')[0].text_content()
-            time = table.xpath('tr[6]/td[2]')[0].text_content()
-            place = table.xpath('tr[6]/td[3]')[0].text_content()
+            date_time_place = table.xpath('tr[6]/td/text()')
+
+            date = date_time_place[0]
+            time = date_time_place[1]
+            place = date_time_place[2]
 
             result.append(
-                {'nome_corso': str(nome_corso),
-                 'codice_corso': str(codice[1:-1]),
-                 'descrizione': str(descrizione),
+                {'nome_corso': nome_corso,
+                 'codice_corso': codice[1:-1],
+                 'descrizione': descrizione,
                  'numero_iscrizione': str(numero_iscrizione),
                  'data': str(date),
                  'ora': str(time),
@@ -36,7 +39,7 @@ class Parser:
 
     def home(self):
         # Custom XPath, thanks Chrome DevTools
-        titolo = str(self.root.xpath('//table[2]/tbody/tr/td/div/text()')[0])
+        titolo = self.root.xpath('//table[2]/tbody/tr/td/div/text()')[0]
 
         # Unpack and capitalize first letter of name
         nome = titolo.split(' - ')[0].title()
@@ -48,15 +51,15 @@ class Parser:
                 if x.strip()]
 
         result = {
-            'nome': str(nome),
-            'matricola': str(matricola),
-            'tipo_di_corso': str(info[0]),
-            'profilo_studente': str(info[1]),
+            'nome': nome,
+            'matricola': matricola,
+            'tipo_di_corso': info[0],
+            'profilo_studente': info[1],
             'anno_di_corso': int(info[2]),
-            'data_immatricolazione': str(info[3]),
-            'corso_di_studio': str(info[4]),
-            'ordinamento': str(info[5]),
-            'percorso_di_studio': str(info[6])
+            'data_immatricolazione': info[3],
+            'corso_di_studio': info[4],
+            'ordinamento': info[5],
+            'percorso_di_studio': info[6]
         }
 
         return result
@@ -71,7 +74,8 @@ class Parser:
         for row in rows:
             # Get text content from td tags with text inside
             # or with a link with text inside
-            cells = [td for td in row.xpath('td/text() | td/a/text()')
+            cells = [td.strip() for td
+                     in row.xpath('td/text() | td/a/text()')
                      if td.strip()]
 
             # Unpack codice, nome from cell
@@ -79,18 +83,16 @@ class Parser:
 
             esame = {
                 'anno_di_corso': int(cells[0]),
-                'nome': str(nome),
-                'codice': str(codice),
+                'nome': nome,
+                'codice': codice,
                 'crediti': int(cells[2]),
-                'anno_frequenza': str(cells[3]),
+                'anno_frequenza': cells[3],
             }
 
             # Check if exam was passed
             if row.xpath('td[img/@alt="Superata"]'):
                 esame['superato'] = True
-                splitted_votodata = cells[4].split(u'\u00a0-\u00a0')
-                esame['voto'] = str(splitted_votodata[0])
-                esame['data'] = str(splitted_votodata[1])
+                esame['voto'], esame['data'] = cells[4].split(u'\u00a0-\u00a0')
             else:
                 esame['superato'] = False
 
@@ -108,7 +110,8 @@ class Parser:
         for idx, row in enumerate(rows):
             # Get text content from td tags with text inside
             # or with a link with text inside
-            cells = [td for td in row.xpath('td/text() | td/a/text()')
+            cells = [td.strip() for td
+                     in row.xpath('td/text() | td/a/text()')
                      if td.strip()]
 
             # This is needed to handle the case of multiple fees,
@@ -125,10 +128,10 @@ class Parser:
 
             tassa = {
                 'codice_fattura': int(cells[0]),
-                'codice_bollettino': str(cells[1]),
-                'anno': str(cells[2]),
-                'descrizione': str(cells[3]),
-                'data_scadenza': str(cells[4]),
+                'codice_bollettino': cells[1],
+                'anno': cells[2],
+                'descrizione': cells[3],
+                'data_scadenza': cells[4],
                 'importo': float(cells[5][2:].replace(',', '.')),
                 'pagata': pagata
             }
@@ -152,18 +155,19 @@ class Parser:
             else:
                 iscrizioni_aperte = True
 
-            cells = [td for td in row.xpath('td/text() | td/a/text()')
+            cells = [td.strip() for td
+                     in row.xpath('td/text() | td/a/text()')
                      if td.strip()]
 
             appello = {
-                'nome_corso': str(cells[0]),
-                'data_esame': str(cells[1]),
+                'nome_corso': cells[0],
+                'data_esame': cells[1],
                 'periodo_iscrizione': {
-                    'inizio': str(cells[2]),
-                    'fine': str(cells[3])
+                    'inizio': cells[2],
+                    'fine': cells[3]
                 },
-                'descrizione': str(cells[4]),
-                'sessioni': str(cells[5]),
+                'descrizione': cells[4],
+                'sessioni': cells[5],
                 'iscrizioni_aperte': iscrizioni_aperte
             }
 
